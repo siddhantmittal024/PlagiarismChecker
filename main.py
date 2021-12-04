eb2d9349afd99fa8b3cfb9a9f55b9b6208239bef
Siddhant Mittal <sidmit2001@gmail.com>
2021-12-04 18:23:21 +0530

    refactor

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import os
-import matplotlib.pyplot as plt
-import ProcessData as pp
-import SimilarityCalculator as cs
+import matplotlib.pyplot as plotGraph
+import logic as PreProcess
+import util as Calculate
 from tkinter import *
 
-l = {}
+docList = {}
 
-def ppcorpus():
+def preProcessData():
     """Preprocess the corpus taking all .txt files in directory
         and calls the preprocessing class for each of them"""
     files = [doc for doc in os.listdir() if (doc.endswith(
-        '.txt') and (doc != 'store.txt' and doc != 'temp.txt'))]
+        '.txt') and (doc != 'DataStore.txt' and doc != 'temp.txt'))]
     for doc in files:
-        l[doc] = pp.Preprocess(doc).trig
+        docList[doc] = PreProcess.Preprocess(doc).trig
 
 
-def storeCorpus():
+def storeCorpusData():
     """ Stores the preprocesed corpus in a text file
         to avoid the computational load of preprocessing
         every time"""
-    target = open('store.txt', "w")
-    target.write(str(l))
-    target.close()
+    trg = open('DataStore.txt', "w")
+    trg.write(str(docList))
+    trg.close()
 
 
-def loadCorpus():
+def loadCorpusData():
     """Loads the preprocessed data"""
-    ter = open('store.txt', 'r')
-    s = ter.read()
-    ter.close()
-    l = eval(s)
-    print('a', l)
+    temp = open('DataStore.txt', 'r')
+    s = temp.read()
+    temp.close()
+    docList = eval(s)
+    print('a', docList)
 
 master = Tk()
 master.title("Plagiarism Checker")
-master.config(bg="#FADA5E")  # title of the window
+master.config(bg="#FADA5E")  
 Label(master, text="Enter file name", font=(
     "Helvetica", 12)).grid(row=0, column=1)
 
@@ -42,22 +42,22 @@ Label(master, text="Enter input text", font=(
     "Helvetica", 12)).grid(row=5, column=1)
 
 master.geometry("750x780")
-e1 = Entry(master)  # text field to input file name
+e1 = Entry(master) 
 
 e1.grid(row=3, column=1, pady=15)
 
-# text field to input text to be checked
+
 t = Text(master, width=80, height=35)
 t.grid(row=6, column=1, padx=50, pady=10)
 
 
-def Prepro():
+def PP():
     """ Calls the preprocessing functions"""
-    ppcorpus()
-    storeCorpus()
+    preProcessData()
+    storeCorpusData()
 
 
-def helloCallBack():  # function that runs when we click the check button
+def graphExecution():  # function that runs when we click the check button
     """ Gets the data from input boxes and preprocesses it
         adds the file to the corpus and finally graphs the
         plagiarism percentage in form of bar graph"""
@@ -67,34 +67,30 @@ def helloCallBack():  # function that runs when we click the check button
     f = open(r, "w")
     f.write(s)
     f.close()
-    inp = pp.Preprocess(r).trig
+    inp = PreProcess.Preprocess(r).trig
     X = []
     Y = []
-    doc_rank = cs.CalcSim(l, inp).doc_rank
-    for i in range(len(l)):
+    doc_rank = Calculate.Logic(docList, inp).rank
+    for i in range(len(docList)):
         item = doc_rank.get()
         Y.append(item[0])
         X.append(item[1])
-    # graph the similarity
 
-    plt.bar(X, Y, width=0.5, align='center')
-    plt.xlabel("Doc Name")
-    plt.ylabel("% Similarity")
-    plt.title("Similarity Analaysis")
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    #plt.grid()
-    plt.show()
-    # master.destroy()
-    '''Output Frame'''
+    plotGraph.bar(X, Y, width=0.5, align='center')
+    plotGraph.xlabel("Doc Name")
+    plotGraph.ylabel("% Similarity")
+    plotGraph.title("Similarity Analaysis")
+    plotGraph.xticks(rotation=90)
+    plotGraph.tight_layout()
+    plotGraph.show()
 
-
+   
 prep = Button(master, text='Process Data', bg='black',
-              fg='white', width=24, command=Prepro)  # check button
+              fg='white', width=24, command=PP) 
 prep.grid(row=8, column=1, pady=10)
 
 check = Button(master, text='Check Plagiarism', bg='black',
-               fg='white', width=24, command=helloCallBack)  # check button
+               fg='white', width=24, command=graphExecution)  
 check.grid(row=12, column=1)
 
 master.mainloop()
